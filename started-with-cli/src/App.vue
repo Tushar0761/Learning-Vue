360ed198b61e6b15c55ef15267a6405ed1aedf6e
Tushar0761 <tusharpanchal0761@gmail.com>
2024-04-12 17:11:58 +0530

    Refs completed

---
+++ started-with-cli/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+
+        <div>
+            <p>{{ count }}</p>
+            <p>
+                <button style="font-size: 2.5rem" @click="inc()">Inc</button>
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            title: "My First vue app using CLI",
+            count: 10,
+        };
+    },
+    methods: {
+        inc() {
+            this.count++;
+        },
+    },
+};
+</script>
+<style>
+h1 {
+    text-align: center;
+    padding: 0 0 5px 0;
+    border: solid black;
+    border-width: 0 0 3px 0;
+}
+p {
+    font-size: 3rem;
+    text-align: center;
+}
+</style>
---
+++ template-ref/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="text-xl text-center">
+        <h1 class="text-3xl">{{ title }}</h1>
+        <div class="border rounded p-2 m0">
+            <input type="text" ref="name" class="border p-1 rounded-lg" />
+            <button class="px-3 m-1 bg-blue-100 rounded" @click="handler()">
+                Click
+            </button>
+            <button class="px-3 m-1 bg-blue-100 rounded" @click="style()">
+                change class
+            </button>
+            <div class="text-center bg-red-300 m-3 p-3" ref="box">
+                <p ref="output"></p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App",
+    data() {
+        return {
+            title: "Working with refs",
+        };
+    },
+    methods: {
+        style() {
+            console.log("style changed");
+            this.$refs.box.classList.replace("bg-red-300", "bg-yellow-500");
+        },
+        handler() {
+            let input = this.$refs.output;
+
+            input.innerHTML += "<br>";
+            console.log(input.innerHTML);
+
+            input.innerHTML += this.$refs.name.value;
+            console.log(input.innerHTML);
+            input.innerHTML += "<br>";
+            console.log(input.innerHTML);
+
+            input.classList.add("py-5");
+        },
+    },
+};
+</script>
