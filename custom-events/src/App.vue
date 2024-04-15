45d52fabc348d28653c9bf95914d438657518a52
Tushar0761 <tusharpanchal0761@gmail.com>
2024-04-15 15:44:29 +0530

    custom event handling is done

---
+++ custom-events/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="text-center relative">
+        <h1 class="text-2xl">Hello</h1>
+        <div v-if="showModel">
+            <ModalComp @close="toggleModal" />
+        </div>
+        <button @click="toggleModal" class="bg-slate-300">Click</button>
+    </div>
+</template>
+
+<script>
+import ModalComp from "./components/ModalComp.vue";
+
+export default {
+    data() {
+        return {
+            showModel: false,
+        };
+    },
+    methods: {
+        toggleModal() {
+            console.log(event);
+            this.showModel = !this.showModel;
+        },
+    },
+    components: { ModalComp },
+};
+</script>
+
+<style></style>
---
+++ custom-events/src/components/ModalComp.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+        @click.self="closeModel"
+        class="absolute top-0 bg-black opacity-80 h-screen w-screen flex justify-center items-center"
+    >
+        <div class="text-black bg-white size-48 rounded-lg">Here</div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        closeModel() {
+            this.$emit("close");
+        },
+    },
+};
+</script>
